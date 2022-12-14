9d0ca25e458c0aef8480ebbc69c19a699aeef69e
IvanGlaz79vvv <ivangl79@gmail.com>
2022-12-15 01:36:37 +0300

    Initial commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,10 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("\nтHello world!");
+        System.out.println("\nHello world!");
+
+
     }
+
+
 }
