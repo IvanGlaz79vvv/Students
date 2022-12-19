f102eb657de30506b9d43b21d693e3b8b36e33d6
IvanGlaz79vvv <ivangl79@gmail.com>
2022-12-19 05:23:41 +0300

    Пишу SessionFactory https://javastudy.ru/hibernate/hibernate-quick-start/

---
+++ src/main/java/com/util/HibernateSessionFactory.java
@@ -0,0 +1,13 @@
+package com.util;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+
+public class HibernateSessionFactory {
+    private static SessionFactory sessionFactory = buildSessionFactory();
+
+    protected static SessionFactory buildSessionFactory(){
+        // A SessionFactory is set up once for an application!
+        final
+    }
+}
